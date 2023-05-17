843a62bad7a9730a07053bbf12e64abcbf166185
hdang09 <dangtranhai628@gmail.com>
2023-05-17 22:11:10 +0700

    ADD - Post, put, delete user function

--- src/main/java/hdang09/controllers/UserController.java
+++ src/main/java/hdang09/controllers/UserController.java
@@ -3,9 +3,12 @@ package hdang09.controllers;
 import hdang09.entites.User;
 import hdang09.services.UserService;
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +24,21 @@ public class UserController {
     @GetMapping("{studentId}")
     public User getUserResult(@PathVariable int studentId) {
         return service.getUserResult(studentId);
+    }
+    
+    @PostMapping("register")
+    public void register(@RequestBody User user) {
+        service.register(user);
+    }
+    
+    // Don't require it
+    @PutMapping("update/{studentId}")
+    public String updateUser(@RequestBody User user, @PathVariable int studentId) {
+        return service.updateUser(user, studentId);
+    }
+    
+    @DeleteMapping("delete/{studentId}")
+    public String deleteUser(@PathVariable int studentId) {
+        return service.deleteUser(studentId);
     }
 }
--- src/main/java/hdang09/services/UserService.java
+++ src/main/java/hdang09/services/UserService.java
@@ -6,6 +6,7 @@ package hdang09.services;
 
 import hdang09.entites.User;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,26 +15,56 @@ import java.util.List;
  */
 public class UserService {
 
-    List<User> users = new ArrayList<>();
+    static List<Integer> answers = new ArrayList<>(Arrays.asList(456, 654, 78, 56));
 
-    public UserService() {
-        List<Integer> answers = new ArrayList<>();
-        answers.add(456);
-        users.add(new User(1, "name", "studentID", 1123, 231, 123, 3, "questions", answers, 23));
-        users.add(new User(2, "name", "studentID", 1123, 231, 123, 3, "questions", answers, 23));
-        users.add(new User(3, "name", "studentID", 1123, 231, 123, 3, "questions", answers, 23));
-        users.add(new User(4, "name", "studentID", 1123, 231, 123, 3, "questions", answers, 23));
-    }
+    static List<User> users = new ArrayList<>(
+            Arrays.asList(
+                    new User(1, "name", "studentID", 1123, 231, 123, 3, "questions", answers, 23),
+                    new User(2, "name", "studentID", 1123, 231, 123, 3, "questions", answers, 23),
+                    new User(3, "name", "studentID", 1123, 231, 123, 3, "questions", answers, 23),
+                    new User(4, "name", "studentID", 1123, 231, 123, 3, "questions", answers, 23)
+            )
+    );
 
     public List<User> getAllUser() {
         return users;
     }
 
     public User getUserResult(int studentId) {
-        for (User user: users) {
-            if (user.getId() == studentId) return user;
+        for (User user : users) {
+            if (user.getId() == studentId) {
+                return user;
+            }
         }
         return null;
     }
 
+    public void register(User user) {
+        users.add(user);
+    }
+
+    public String updateUser(User updateduser, int studentId) {
+        String result = "";
+        
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId() == studentId) {
+                updateduser.setId(studentId);
+                users.set(i, updateduser);
+                result = "Updated";
+            }
+        }
+        return result.isEmpty() ? "Can't find studentId" : result;
+    }
+    
+    public String deleteUser(int studentId) {
+        String result = "";
+        
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId() == studentId) {
+                users.remove(i);
+                result = "Deleted";
+            }
+        }
+        return result.isEmpty() ? "Can't find studentId" : result;
+    }
 }
