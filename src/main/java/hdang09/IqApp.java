eb40781d081bf4196fd23b5d1a63a039c8d32c1e
hdang09 <dangtranhai628@gmail.com>
2023-05-22 16:13:26 +0700

    FIX - Fix global CORS

--- src/main/java/hdang09/IqApp.java
+++ src/main/java/hdang09/IqApp.java
@@ -3,6 +3,8 @@ package hdang09;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //import springfox.documentation.builders.RequestHandlerSelectors;
 //import springfox.documentation.spi.DocumentationType;
 //import springfox.documentation.spring.web.plugins.Docket;
@@ -15,6 +17,16 @@ public class IqApp {
     public static void main(String[] args) {
         SpringApplication.run(IqApp.class, args);
     }
+    
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("https://iq-api.up.railway.app");
+			}
+		};
+	}
 
 //    @Bean
 //    public Docket productApi() {
