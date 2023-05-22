6028dd6558b95d2c6a1f568c8feb187ba9553ca4
hdang09 <dangtranhai628@gmail.com>
2023-05-22 15:55:08 +0700

    FIX - Add CORS for each method in controller

--- src/main/java/hdang09/controllers/UserController.java
+++ src/main/java/hdang09/controllers/UserController.java
@@ -18,39 +18,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://iq-api.up.railway.app", maxAge = 3600)
+@CrossOrigin(maxAge = 3600)
 @RequestMapping("user")
 public class UserController {
 
     @Autowired
     UserService service = new UserService();
 
+    @CrossOrigin(origins = "https://iq-api.up.railway.app")
     @GetMapping("scoreboard")
     public List<User> getAllUser() {
         return service.getAllUser();
     }
 
+    @CrossOrigin(origins = "https://iq-api.up.railway.app")
     @GetMapping("{studentId}")
     public User getUserResult(@PathVariable String studentId) {
         return service.getUserResult(studentId);
     }
 
+    @CrossOrigin(origins = "https://iq-api.up.railway.app")
     @PostMapping("register")
     public ResponseEntity<CustomResponse> register(@RequestBody User newUser) {
         return service.register(newUser);
     }
 
+    @CrossOrigin(origins = "https://iq-api.up.railway.app")
     @GetMapping("start/{name}/{studentID}")
     public ResponseEntity<CustomResponse> startTheQuiz(@PathVariable String name, @PathVariable String studentID) {
         return service.startTheQuiz(studentID);
     }
 
+    @CrossOrigin(origins = "https://iq-api.up.railway.app")
     @PutMapping("end")
     public ResponseEntity<CustomResponse> submitTheQuiz(@RequestBody CustomUser user) {
         return service.submitTheQuiz(user);
     }
 
     // Don't require it
+    @CrossOrigin(origins = "https://iq-api.up.railway.app")
     @PutMapping("update/{studentId}")
     public void updateUser(@RequestBody User user, @PathVariable String studentId) {
         if (studentId != null) {
@@ -59,6 +65,7 @@ public class UserController {
         service.updateUser(user);
     }
 
+    @CrossOrigin(origins = "https://iq-api.up.railway.app")
     @DeleteMapping("delete/{studentId}")
     public void deleteUser(@PathVariable String studentId) {
         service.deleteUser(studentId);
