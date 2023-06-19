d486d959fabc898659fd6d96b4f8c829eecfa919
hdang09 <dangtranhai628@gmail.com>
2023-06-19 12:48:29 +0700

    DELETE - Delete Docker file

--- src/main/java/hdang09/entities/User.java
+++ src/main/java/hdang09/entities/User.java
@@ -55,14 +55,6 @@ public class User {
         this.rank = rank;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
